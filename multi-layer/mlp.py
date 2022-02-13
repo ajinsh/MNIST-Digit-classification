9d623d9cef2e910e4fd5f2160911551ed910d385
Ajay Babu Gorantla <gorantlaajayb@gmail.com>
2022-02-12 18:06:16 -0800

    refactored the whole code using helper_functions():with proper naming convention, function descriptions, added datasets in zipped folder

---
+++ multi-layer/mlp.py
@@ -0,0 +1,9 @@
+from helper_functions import *
+
+def mlp_test(biased_X_test, Y_test_target, W1, W2):
+    pass
+
+def mlp(biased_X_train, Y_train_target, biased_X_test, Y_test_target,hidden_units, epochs, learning_rate, momentum):
+    pass
+
+
